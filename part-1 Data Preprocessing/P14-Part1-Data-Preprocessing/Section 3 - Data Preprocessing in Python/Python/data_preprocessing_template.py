fb24bf550cb1eb0e4606a635499e88856c71e31b
sumit <mandal.sumit00@gmail.om>
2021-02-16 18:51:41 +0530

    hola

---
+++ part-1 Data Preprocessing/P14-Part1-Data-Preprocessing/Section 3 - Data Preprocessing in Python/Python/data_preprocessing_template.py
@@ -0,0 +1,19 @@
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Data.csv')
+X = dataset.iloc[:,:-1].values #it means we take all rows and all columnns except last
+Y = dataset.iloc[:,3].values #it shows all rows and only 3 column
+
+#taking care of missing data
+from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
+imputer = imputer.fit(X[:, 1:3]) #used for fitting imputer object to matrix X
+X[:, 1:3] = imputer.transform(X[:, 1:3]) #X[:, 1:3] variable is used to take columns for missing data
+#imputer.transform(X[:, 1:3]) is method which is used to replace missing values with mean.
+print(X)
