c4853b9772641d23113ae02aa45c0b2fa8fec1a7
sumit <mandal.sumit00@gmail.om>
2021-02-19 10:53:48 +0530

    10:53/19-02-2021

--- Part2-Regression/Section 6 - Simple Linear Regression/Python/simple_linear_regression.py
+++ Part2-Regression/Section 6 - Simple Linear Regression/Python/simple_linear_regression.py
@@ -20,3 +20,32 @@ X_train,X_test, y_train, y_test =  train_test_split(X, y, test_size = 1/3, rando
 #X_test is matrix of indepenndent variable and y_test is dependent variable factor for test set
 
 #fitting Simple Linear Regression into the training set
+
+from sklearn.linear_model import LinearRegression#odinary least ssquares linear regression
+regressor = LinearRegression()
+regressor.fit(X_train,y_train)
+
+#predicting the test results
+y_pred = regressor.predict(X_test) #y_pred is vector of predictions of dependent variable
+ #it will contain predicted salary of all the test set. since dependent variable is salary 
+ 
+ #ypred contains predicted sal and y_test actual salary
+ 
+ #predictionn of training set
+y_pred_train = regressor.predict(X_train)
+ 
+ #visualising the trainig set results
+plt.scatter(X_train,y_train,color='red') #obbservaton points
+plt.plot(X_train,y_pred_train,color = 'blue')
+plt.title('salary vs experience(Training set)')
+plt.xlabel('years of expirience')
+plt.ylabel('salary')
+plt.show()
+
+#visualising test set results
+plt.scatter(X_test,y_test,color='red')
+plt.plot(X_train,regressor.predict(X_train)) #since test_set + train_set =1 line will be same
+plt.title('salary vs experience(Test set)')
+plt.xlabel('years of experience')
+plt.ylabel('salary')
+plt.show()
