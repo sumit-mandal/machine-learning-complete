2f90f2ae679e2d51edb9c2f795494faef7444df2
sumit <mandal.sumit00@gmail.om>
2021-02-21 14:00:11 +0530

    13:59/21-02-21

--- Part2-Regression/Section 7 - Multiple Linear Regression/Python/multiple_linear_regression.py
+++ Part2-Regression/Section 7 - Multiple Linear Regression/Python/multiple_linear_regression.py
@@ -5,29 +5,64 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-print(X)
+#importing the dataset
 
-# Encoding categorical data
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:,:-1]
+y = dataset.iloc[:,-1]
+
+#using label encoders to covert labels (california,ny,florida...) to numbers
+
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough') #one hot encode in third column
 X = np.array(ct.fit_transform(X))
 print(X)
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Training the Multiple Linear Regression model on the Training set
+#splitting the dataset into training and test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train,y_test = train_test_split(X,y, test_size = 0.2, random_state = 0)
+
+#fitting Multiple Linear Regression into the training set
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+regressor.fit(X_train,y_train)
 
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
-np.set_printoptions(precision=2)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+#predicting the test Set results
+y_pred = regressor.predict(X_test) 
+
+#Building the optimal model using backward Elimination
+import statsmodels.formula.api as sm
+import statsmodels.api as sm
+X = np.append(arr = np.ones((50,1)).astype(int),values=X ,axis=1) 
+#we have added one in 0th column
+
+X_opt = X[:,[0,1,2,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+#ols means odiniary least square
+
+#we include all the independent variable at first and then we will remove one by one,the independent variables that are not statistically significant
+
+X_opt = X[:,[0,1,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+X_opt = X[:,[0,3,4,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+X_opt = X[:,[0,3,5]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+X_opt = X[:,[0,3]]
+regressor_OLS = sm.OLS(endog = y, exog = X_opt).fit()
+regressor_OLS.summary()
+
+
+
