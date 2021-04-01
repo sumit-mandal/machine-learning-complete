a8a06d603755369c3cd887fd19b6740b905f0018
sumit <mandal.sumit00@gmail.om>
2021-04-01 20:41:20 +0530

    20:41/01/04/21

--- Part2-Regression/Section 9 - Support Vector Regression (SVR)/R/data_preprocessing_template.R
+++ Part2-Regression/Section 9 - Support Vector Regression (SVR)/R/data_preprocessing_template.R
@@ -1,16 +1,28 @@
-# Data Preprocessing Template
+#SVR
 
-# Importing the dataset
-dataset = read.csv('Data.csv')
+#importing the dataset
+dataset = read.csv('Position_Salaries.csv')
+dataset = dataset[2:3]
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
-library(caTools)
-set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
-training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+#Fitting the svr to the dataset
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+#install.packages('e1071')
+library(e1071)
+regressor = svm(formula = Salary ~ .,
+                data = dataset,
+                type = 'eps-regression')
+
+
+#predicting the new result
+y_pred = predict(regressor,data.frame(Level = 6.5))
+
+#Visualising the Svr result
+library(ggplot2)
+ggplot()+
+  geom_point(aes(x = dataset$Level, y = dataset$Salary),
+             colour = 'red') + 
+  geom_line(aes(x=dataset$Level, y = predict(regressor,newdata = dataset)),
+            colour = 'blue') + 
+  ggtitle('Truth or Bluff(SVR)') +
+  xlab('Level')+
+  ylab('Salary')
