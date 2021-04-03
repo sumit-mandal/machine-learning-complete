fdae804d1c2823d388274d8b44f1d3bd14fc8ac5
sumit <mandal.sumit00@gmail.om>
2021-04-03 11:28:38 +0530

    11:28,03-04-2021

--- Part2-Regression/Section 10 - Decision Tree Regression/Python/decision_tree_regression.py
+++ Part2-Regression/Section 10 - Decision Tree Regression/Python/decision_tree_regression.py
@@ -5,25 +5,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+#importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
-y = dataset.iloc[:, -1].values
+X = dataset.iloc[:, 1:2].values
+y = dataset.iloc[:, 2].values
 
-# Training the Decision Tree Regression model on the whole dataset
+#Fitting the decision tree Regression to the dataset
 from sklearn.tree import DecisionTreeRegressor
-regressor = DecisionTreeRegressor(random_state = 0)
-regressor.fit(X, y)
+regressor = DecisionTreeRegressor(random_state= 0)
+regressor.fit(X,y)
 
-# Predicting a new result
-regressor.predict([[6.5]])
+#Predicting a new result
+y_pred = regressor.predict([[6.5]])
 
-# Visualising the Decision Tree Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Decision Tree Regression)')
-plt.xlabel('Position level')
+#Visualising the decision tree regression reults
+X_grid = np.arange(min(X),max(X),0.01)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y,color='red')
+plt.plot(X_grid,regressor.predict(X_grid),color='blue')
+plt.title('Truth or bluff(Regresion model)')
+plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
