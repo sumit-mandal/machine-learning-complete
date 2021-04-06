ccf6f0569f8cfa2172784e7585a7f23f20d10e7f
sumit <mandal.sumit00@gmail.om>
2021-04-06 21:10:09 +0530

    21:09/06-04-21

--- Part2-Regression/Section 10 - Decision Tree Regression/R/decision_tree_regression.R
+++ Part2-Regression/Section 10 - Decision Tree Regression/R/decision_tree_regression.R
@@ -1,44 +1,28 @@
-# Decision Tree Regression
+#Decision tree regression
 
-# Importing the dataset
+#importing the dataset
 dataset = read.csv('Position_Salaries.csv')
 dataset = dataset[2:3]
 
-# Splitting the dataset into the Training set and Test set
-# # install.packages('caTools')
-# library(caTools)
-# set.seed(123)
-# split = sample.split(dataset$Salary, SplitRatio = 2/3)
-# training_set = subset(dataset, split == TRUE)
-# test_set = subset(dataset, split == FALSE)
+#Fitting decision tree regression to the dataset
 
-# Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
-
-# Fitting Decision Tree Regression to the dataset
-# install.packages('rpart')
+# install.packages("rpart")
 library(rpart)
-regressor = rpart(formula = Salary ~ .,
+regressor = rpart(formula=Salary~.,
                   data = dataset,
                   control = rpart.control(minsplit = 1))
 
-# Predicting a new result with Decision Tree Regression
-y_pred = predict(regressor, data.frame(Level = 6.5))
+#predicting a new result
+y_pred = predict(regressor,data.frame(Level=6.5))
 
-# Visualising the Decision Tree Regression results (higher resolution)
-# install.packages('ggplot2')
+#visualising the plot
 library(ggplot2)
-x_grid = seq(min(dataset$Level), max(dataset$Level), 0.01)
+x_grid = seq(min(dataset$Level), max(dataset$Level), 0.1)
 ggplot() +
   geom_point(aes(x = dataset$Level, y = dataset$Salary),
              colour = 'red') +
   geom_line(aes(x = x_grid, y = predict(regressor, newdata = data.frame(Level = x_grid))),
             colour = 'blue') +
-  ggtitle('Truth or Bluff (Decision Tree Regression)') +
+  ggtitle('Truth or Bluff (Regression Model)') +
   xlab('Level') +
   ylab('Salary')
-
-# Plotting the tree
-plot(regressor)
-text(regressor)
