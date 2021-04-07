4635130d67b59fdfa7897ec1fb277aea03d1de61
sumit <mandal.sumit00@gmail.om>
2021-04-07 18:51:51 +0530

    18:51/07-04-2021

--- Part2-Regression/Section 11 - Random Forest Regression/Python/random_forest_regression.py
+++ Part2-Regression/Section 11 - Random Forest Regression/Python/random_forest_regression.py
@@ -1,29 +1,30 @@
 # Random Forest Regression
 
 # Importing the libraries
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+#importing the dataset
 dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
-y = dataset.iloc[:, -1].values
+X = dataset.iloc[:,1:2].values
+y = dataset.iloc[:,2].values
 
-# Training the Random Forest Regression model on the whole dataset
+#Fitting Random Forest Regressioin to the dataset
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
-regressor.fit(X, y)
+regressor = RandomForestRegressor(n_estimators = 1000,criterion='mse',random_state=0)
+regressor.fit(X,y)
 
-# Predicting a new result
-regressor.predict([[6.5]])
+#predicting the new result
+y_pred = regressor.predict([[6.5]])
 
-# Visualising the Random Forest Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
-plt.xlabel('Position level')
+#Visualising the result
+X_grid = np.arange(min(X),max(X),0.1)
+X_grid = X_grid.reshape((len(X_grid),1))
+plt.scatter(X,y,color = 'red')
+plt.plot(X_grid,regressor.predict(X_grid),color='blue')
+plt.title('Truth or bluff(Random Forest Regression)')
+plt.xlabel('Position Level')
 plt.ylabel('Salary')
 plt.show()
