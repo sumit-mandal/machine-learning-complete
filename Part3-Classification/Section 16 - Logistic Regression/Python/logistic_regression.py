2c0634bbbe2c72505cad0c397b4561163fb8d078
sumit <mandal.sumit00@gmail.om>
2021-04-09 11:47:49 +0530

    11:47/09-04-21

--- Part3-Classification/Section 16 - Logistic Regression/Python/logistic_regression.py
+++ Part3-Classification/Section 16 - Logistic Regression/Python/logistic_regression.py
@@ -35,6 +35,54 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
 
-
 #it evaluates if our logistic regression model learnt and understood correctly the corelation in training sets
 # and see if it can makes powerful prediction.
+
+
+#Visualising the training set results
+from matplotlib.colors import ListedColormap
+X_set,y_set = X_train,y_train
+X1,X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max() + 1, step=0.01 ),#it is for age
+                    np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max() + 1, step=0.01)
+                    ) 
+#We take min-1 and max+1 so that point don't gets squeezed thus making minimumand maximumum value to be -2 and 3 respectively
+#we prepare the grid with all the pixel points
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha=0.75,cmap = ListedColormap(('red','green')))
+#it colorizes all the pixel. and draws the contour line between those who bought and didn't buy
+#predict function is use to predict wether each classifier belongs to class 0 or class 1. If pixel point belongs to class 0 it will be colorized red and
+#if it belongs to 1 it will be colorized green.
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+ #sets the limit for current axes
+ 
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j,0],X_set[y_set == j,1],
+                c = ListedColormap(('blue','black'))(i),label = j)
+#with this loop we plot all the datapoints.
+plt.title('Logistic Regression(Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+#visualising the test set result
+from matplotlib.colors import ListedColormap
+X_set,y_set = X_test,y_test
+X1,X2 = np.meshgrid(np.arange(start = X_set[:,0].min()-1, stop = X_set[:,0].max() + 1,step = 0.01),
+                    np.arange(start = X_set[:,1].min()-1, stop = X_set[:,1].max() + 1,step = 0.01)
+                    )
+plt.contourf(X1,X2,classifier.predict(np.array([X1.ravel(),X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75,cmap = ListedColormap(('blue','black')))
+plt.xlim(X1.min(),X1.max())
+plt.ylim(X2.min(),X2.max())
+
+for i,j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j,0],X_set[y_set== j,1],
+                c = ListedColormap(('red','green'))(i),label=j)
+    
+plt.title('Logistic Regression(Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
